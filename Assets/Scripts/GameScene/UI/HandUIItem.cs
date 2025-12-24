5dec84233a5a9c2668f9f69893248339ca98d78c
Sujith Nair <sujithaturservice@gmail.com>
2025-12-24 19:41:37 +0530

    Added UI + Game Scene Changes

---
+++ Assets/Scripts/GameScene/UI/HandUIItem.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using Core.DTOs;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameScene.UI
+{
+    internal class HandUIItem : MonoBehaviour
+    {
+        [SerializeField] private Image imageSlot;
+        [SerializeField] private TextMeshProUGUI text;
+
+        private HandUIData _data;
+        
+        internal void Initialize(HandUIData data)
+        {
+            _data = data;
+            if (text != null ) text.text = data.Name;
+            if (imageSlot != null ) imageSlot.sprite = data.Sprite;
+        }
+    }
+}
---
+++ Assets/Scripts/GameScene/UI/HandsUIHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace GameScene.UI
+{
+    internal class HandsUIHandler : MonoBehaviour
+    {
+        
+    }
+}
